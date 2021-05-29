f891cf6d262be61d3b7948eaca1b24bcfdb0f6d5
faisal nawaz saifi <faisalnawaz7570@gmail.com>
2021-05-29 23:32:42 +0500

    mongoDB Atlas connected

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const mongoDBPassword = "FBQEqyEtMVhIELfk"
+
+mongoose.connect('mongodb+srv://Faisalnawaz:FBQEqyEtMVhIELfk@cluster0.jyfng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then((con) => {
+    console.log("connected to mongoDb");
+}); 
 
 const app = express();
 
